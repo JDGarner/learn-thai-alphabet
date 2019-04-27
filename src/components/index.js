3ad9e1b9e6b98f637d6265a914da7ee514c3afb3
jamieg <jamie.garner@caplin.com>
2019-04-27 20:26:44 +0700

    more styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,13 @@
 import { SmallText, MediumText, LargeText } from "./text/Text";
 import { SmallButton, MediumButton, LargeButton } from "./button/Button";
+import { CenteredContainer } from "./containers/Containers";
 
-export { SmallText, MediumText, LargeText, SmallButton, MediumButton, LargeButton };
+export {
+  SmallText,
+  MediumText,
+  LargeText,
+  SmallButton,
+  MediumButton,
+  LargeButton,
+  CenteredContainer
+};
--- src/features/quiz/Question.js
+++ src/features/quiz/Question.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { View, Text, TouchableHighlight, TextInput } from "react-native";
-import { LargeText, MediumText, MediumButton } from "../../components";
+import { View, Text, TextInput } from "react-native";
+import { LargeText, MediumText, MediumButton, CenteredContainer } from "../../components";
 
 const GivenText = styled(LargeText)`
   text-align: center;
+`;
+
+const QuestionContainer = styled(View)`
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const QuestionDetails = styled(CenteredContainer)`
+  margin-top: 220px;
+`;
+
+const SubmitButton = styled(MediumButton)`
+  margin-bottom: 220px;
 `;
 
 export default class Question extends Component {
@@ -28,15 +43,16 @@ export default class Question extends Component {
     const { userAnswerText } = this.state;
 
     return (
-      <View>
-        {/* <CenteredText>{givenText}</CenteredText> */}
-        <GivenText>{givenText}</GivenText>
-        <MediumText>{`(${phonetic})`}</MediumText>
-        <TextInput onChangeText={this.onChangeText} value={userAnswerText} />
-        <MediumButton onPress={() => onSubmitAnswer(userAnswerText)}>
+      <QuestionContainer>
+        <QuestionDetails>
+          <GivenText>{givenText}</GivenText>
+          <MediumText>{`(${phonetic})`}</MediumText>
+          <TextInput onChangeText={this.onChangeText} value={userAnswerText} />
+        </QuestionDetails>
+        <SubmitButton onPress={() => onSubmitAnswer(userAnswerText)}>
           <Text>Submit</Text>
-        </MediumButton>
-      </View>
+        </SubmitButton>
+      </QuestionContainer>
     );
   }
 }
